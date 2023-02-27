076b14a7a87f5e80949a053e2f8c296143410652
Salman Jabbari <olddealers@gmail.com>
2023-02-27 12:38:02 +0330

    change

--- index.js
+++ index.js
@@ -1,102 +1,72 @@
-const subList = [
-    "https://raw.githubusercontent.com/freefq/free/master/v2",
-    "https://raw.githubusercontent.com/Pawdroid/Free-servers/main/sub"
-  ]
-  const cnfList = [
-    "https://raw.githubusercontent.com/mahdibland/ShadowsocksAggregator/master/sub/sub_merge.txt"
-  ]
-  
-  
-  const cleanIPLink = "http://bot.sudoer.net/best.cf.iran.all"
-  const operatorList = ["AST", "HWB", "IRC", "MBT", "MCI", "MKB", "PRS", "RTL", "SHT", "ZTL"]
-  const addressList = ["discord.com", "cloudflare.com", "nginx.com", "cdnjs.com", "vimeo.com", "networksolutions.com", "spotify.com"]
-  const fpList = ["chrome", "chrome", "chrome", "firefox", "safari", "edge", "ios", "android", "random", "random"]
-  const alpnList = ["http/1.1", "h2,http/1.1", "h2,http/1.1"]
-  
-  export default {
-    async fetch(request) {
-      var url = new URL(request.url)
-      var pathParts = url.pathname.replace(/^\/|\/$/g, "").split("/")
-      if (pathParts[0] == "sub") {
-        var cleanIPs = []
-        if (pathParts[1] !== undefined) {
-          var operator = pathParts[1].toUpperCase()
-          if (operatorList.includes(operator)) {
-            cleanIPs = await fetch(cleanIPLink).then(r => r.text()).then(t => t.split("\n"))
-            cleanIPs = cleanIPs.filter(line => (line.search(operator) > 0))
-            cleanIPs = cleanIPs.map(line => line.split(" ")[0].trim())
-          }
-        }
-        var configList = []
-        for (var subLink of subList) {
-          configList = configList.concat(await fetch(subLink).then(r => r.text()).then(a => atob(a)).then(t => t.split("\n")))
-        }
-        for (var cnfLink of cnfList) {
-          configList = configList.concat(await fetch(cnfLink).then(r => r.text()).then(t => t.split("\n")))
-        }
-        // configList = configList.concat(await fetch(subLink1).then(r => r.text()).then(a => atob(a)).then(t => t.split("\n")))
-        // configList = configList.concat(await fetch(subLink2).then(r => r.text()).then(a => atob(a)).then(t => t.split("\n")))
-        // configList = configList.concat(await fetch(cnfLink1).then(r => r.text()).then(t => t.split("\n")))
-        configList = configList.filter(cnf => (cnf.search("vmess://") == 0))
-        configList = configList.map(config => {
-          try {
-            var conf = JSON.parse(atob(config.substr(8)))
-            if (conf.tls != "tls") {
-              throw "no-tls"
-            }
-            var addr = conf.sni
-            if (!addr) {
-              if (conf.add && !isIp(conf.add)) {
-                addr = conf.add
-              } else if (conf.host && !isIp(conf.host)) {
-                addr = conf.host
-              }
-            }
-            if (!addr) {
-              throw "no-tls"
-            }
-            conf.add = addressList[Math.floor(Math.random() * addressList.length)]
-            conf.sni = url.hostname
-            if (cleanIPs.length) {
-              conf.host = cleanIPs[Math.floor(Math.random() * cleanIPs.length)]
+const fetch = require('node-fetch');
+
+const subLink = 'https://raw.githubusercontent.com/mahdibland/ShadowsocksAggregator/master/sub/sub_merge.txt';
+
+module.exports = {
+  async fetch(request) {
+    let url = new URL(request.url);
+    let realhostname = url.pathname.split('/')[1];
+    let realpathname = url.pathname.split('/')[2];
+    if (url.pathname.startsWith('/sub')) {
+      let newConfigs = '';
+      let resp = await fetch(subLink);
+      let subConfigs = await resp.text();
+      subConfigs = subConfigs.split('\n');
+      for (let subConfig of subConfigs) {
+        if (subConfig.search('vmess') != -1) {
+          subConfig = subConfig.replace('vmess://', '');
+          subConfig = Buffer.from(subConfig, 'base64').toString('utf-8');
+          subConfig = JSON.parse(subConfig);
+          if (subConfig.sni && !isIp(subConfig.sni) && subConfig.net == 'ws' && subConfig.port == 443) {
+            var configNew = new Object();
+            configNew.v = '2';
+            configNew.ps = 'Node-' + subConfig.sni;
+            if (realpathname == '') {
+              configNew.add = url.hostname;
             } else {
-              conf.host = url.hostname
-            }
-            conf.path = "/" + addr + ":" + conf.port + conf.path
-            conf.fp = fpList[Math.floor(Math.random() * fpList.length)]
-            conf.alpn = alpnList[Math.floor(Math.random() * alpnList.length)]
-            conf.port = 443
-            return "vmess://" + btoa(JSON.stringify(conf))
-          } catch (e) {
-            return ""
+              configNew.add = realpathname;
+            };
+            configNew.port = subConfig.port;
+            configNew.id = subConfig.id;
+            configNew.net = subConfig.net;
+            configNew.host = url.hostname;
+            configNew.path = '/' + subConfig.sni + subConfig.path;
+            configNew.tls = subConfig.tls;
+            configNew.sni = url.hostname;
+            configNew.aid = '0';
+            configNew.scy = 'auto';
+            configNew.type = 'auto';
+            configNew.fp = 'chrome';
+            configNew.alpn = 'http/1.1';
+            configNew = 'vmess://' + Buffer.from(JSON.stringify(configNew)).toString('base64');
+            newConfigs = newConfigs + configNew + '\n';
           }
-        })
-        configList = getMultipleRandomElements(configList.filter(cnf => (cnf.length > 10)), 100)
-        return new Response(btoa(configList.join("\n")));
-      } else {
-        var url = new URL(request.url)
-        var newUrl = new URL("https://" + url.pathname.replace(/^\/|\/$/g, ''))
-        return fetch(new Request(newUrl, request));
+        }
       }
+      return new Response(newConfigs);
+    } else {
+      const url = new URL(request.url);
+      const splitted = url.pathname.replace(/^\/*/, '').split('/');
+      const address = splitted[0];
+      url.pathname = splitted.slice(1).join('/');
+      url.hostname = address;
+      url.protocol = 'https';
+      return fetch(new Request(url, request));
     }
-  }
-  
-  function getMultipleRandomElements(arr, num) {
-    var shuffled = [...arr].sort(() => 0.5 - Math.random());
-    return shuffled.slice(0, num);
-  }
-  
-  function isIp(str) {
-    try {
-      if (str == "" || str == undefined) return false;
-      if (!/^(\d{1,2}|1\d\d|2[0-4]\d|25[0-5])(\.(\d{1,2}|1\d\d|2[0-4]\d|25[0-5])){2}\.(\d{1,2}|1\d\d|2[0-4]\d|25[0-4])$/.test(str)) {
-        return false;
-      }
-      var ls = str.split('.');
-      if (ls == null || ls.length != 4 || ls[3] == "0" || parseInt(ls[3]) === 0) {
-        return false;
-      }
-      return true;
-    } catch (ee) { }
-    return false;
-  }
+  },
+};
+
+function isIp(ipstr) {
+  try {
+    if (ipstr == "" || ipstr == undefined) return false;
+    if (!/^(\d{1,2}|1\d\d|2[0-4]\d|25[0-5])(\.(\d{1,2}|1\d\d|2[0-4]\d|25[0-5])){2}\.(\d{1,2}|1\d\d|2[0-4]\d|25[0-4])$/.test(ipstr)) {
+      return false;
+    }
+    var ls = ipstr.split('.');
+    if (ls == null || ls.length != 4 || ls[3] == "0" || parseInt(ls[3]) === 0) {
+      return false;
+    }
+    return true;
+  } catch (ee) { }
+  return false;
+}
